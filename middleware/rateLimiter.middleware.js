266307acab04fce23339c3b482081a027891d8dd
Kamal Prakash Verma <kamalprakashverma@Kamals-MacBook-Air.local>
2024-12-28 20:59:36 +0530

    increase rateLimiter time

--- middleware/rateLimiter.middleware.js
+++ middleware/rateLimiter.middleware.js
@@ -1,7 +1,7 @@
 import rateLimit from "express-rate-limit";
 const rateLimiter = rateLimit({
     windowMs:60*60*1000,
-    max:10,
+    max:100,
     statusCode:429,
     message:"Too many requests from this IP, please try again after an hour!"
 })
